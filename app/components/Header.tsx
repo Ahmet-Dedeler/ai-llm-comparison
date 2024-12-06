c838713d1d889a46cc49425f70a2cdad86cacbe4
PrettyXD <ahmetemre617@gmail.com>
2024-12-06 19:29:23 +0300

    ph banner

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -15,19 +15,6 @@ export default function Header() {
             GitHub
           </a>
         </Button>
-        <Button variant="outline" size="sm" asChild className="border-black text-black hover:bg-gray-100">
-          <a href="https://supabase.com/blog/supabase-lw12-hackathon" target="_blank" rel="noopener noreferrer">
-            <svg
-              className="mr-2 h-4 w-4"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M21.362 9.354H12V.396a.396.396 0 0 0-.716-.233L2.203 12.424l-.401.562a1.04 1.04 0 0 0 .836 1.659H12v8.959a.396.396 0 0 0 .716.233l9.081-12.261.401-.562a1.04 1.04 0 0 0-.836-1.659z"/>
-            </svg>
-            Supabase Launch Week 12
-          </a>
-        </Button>
         {/* ... (other buttons) ... */}
       </div>
     </header>
