d1f0f6d519a5fc25f06d49426a263a3a88cbac57
Ahmet Dedeler <ahmet-dedeler@users.noreply.github.com>
2025-01-28 00:47:38 +0000

    Update AI models

--- app/utils/aiModels.ts
+++ app/utils/aiModels.ts
@@ -4804,6 +4804,23 @@ export const aiModels: AIModels = {
       }
     },
     {
+      "name": "openrouter/deepseek/deepseek-r1",
+      "provider": "openrouter",
+      "logo": "/logos/openrouter.svg",
+      "sample_spec": {
+        "max_tokens": 8192,
+        "max_input_tokens": 64000,
+        "max_output_tokens": 8192,
+        "input_cost_per_token": "0.00000055",
+        "output_cost_per_token": "0.00000219",
+        "litellm_provider": "openrouter",
+        "mode": "chat",
+        "supports_function_calling": true,
+        "supports_parallel_function_calling": false,
+        "supports_vision": false
+      }
+    },
+    {
       "name": "openrouter/deepseek/deepseek-chat",
       "provider": "openrouter",
       "logo": "/logos/openrouter.svg",
